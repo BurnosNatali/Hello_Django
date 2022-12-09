860eeb7ad164918b4b45f95c2389f1843b7050db
NB <suvorova.natalli.11@yandex.by>
2022-12-09 22:45:42 +0200

    start_files

---
+++ hello_django_1/hello_django_1/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def about(request):
+    a = 345 / 45
+    return render(request, "about.html", {'getting':a})
+
+def home(request):
+    return HttpResponse("This is my home.")
