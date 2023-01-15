b0c8eb778e7b1720e80ae6e28111aed12dc3fbfb
Natali Burnos <110636288+BurnosNatali@users.noreply.github.com>
2023-01-15 09:57:08 +0200

    Update views.py

--- hello_django_1/hello_django_1/views.py
+++ hello_django_1/hello_django_1/views.py
@@ -6,4 +6,4 @@ def about(request):
     return render(request, "about.html", {'getting':a})
 
 def home(request):
-    return HttpResponse("Здравствуй, дом!")
+    return HttpResponse("Здравствуйте!Ура,я дом!")
